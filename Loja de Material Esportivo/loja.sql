dbaf68c171133bb031dd8f6a9cdc40c9a7969f22
seocodes <azanettev@gmail.com>
2025-04-14 13:11:14 -0300

    o banco ai

---
+++ Loja de Material Esportivo/loja.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tempo de Geração: 14/04/2025 às 18h07min
+-- Versão do Servidor: 5.5.20
+-- Versão do PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `loja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`codigo`, `nome`) VALUES
+(1, 'Masculino'),
+(2, 'Feminino'),
+(3, 'Infantil'),
+(4, 'Unissex');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marca`
+--
+
+CREATE TABLE IF NOT EXISTS `marca` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `marca`
+--
+
+INSERT INTO `marca` (`codigo`, `nome`) VALUES
+(1, 'Nike'),
+(2, 'Adidas'),
+(3, 'Olympikus'),
+(4, 'Puma'),
+(5, 'Mizuno'),
+(6, 'Growth');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE IF NOT EXISTS `produto` (
+  `codigo` int(5) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `tamanho` varchar(10) NOT NULL,
+  `preco` float(10,2) NOT NULL,
+  `codmarca` int(5) NOT NULL,
+  `codcategoria` int(5) NOT NULL,
+  `codtipo` int(5) NOT NULL,
+  `foto1` varchar(100) NOT NULL,
+  `foto2` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `codmarca` (`codmarca`),
+  KEY `codcategoria` (`codcategoria`),
+  KEY `codtipo` (`codtipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`codigo`, `descricao`, `cor`, `tamanho`, `preco`, `codmarca`, `codcategoria`, `codtipo`, `foto1`, `foto2`) VALUES
+(1, 'Chinelo Adidas', 'Preto e branco', '42', 89.99, 2, 4, 2, '7ba16df680e3701b58c53c9b221fef83', 'cf78288f3c33edf8f5200160059d4162'),
+(2, 'Chinelo Nike', 'Preto', '40', 59.99, 1, 4, 2, 'd002ad8cc8dcece1bafdbd74d6e5b448', '2c9017a20a0d5d677ffa0f55c35cf119'),
+(3, 'Ã“culos Adidas', 'Preto', 'U', 179.99, 2, 4, 3, '44dcd21e7b906ee8521f3426c23c3739', '52b11ff55c375760c022e8d1fc38d9b0'),
+(4, 'Camisa Growth', 'Preto', 'GG', 99.99, 6, 1, 1, '73bd065a0145efd0090ae0185b3acf6a', 'bc0a0989c3149cbffc0f4f3f4afdae7b'),
+(5, 'CalÃ§a Mizuno', 'Preto', 'M', 129.99, 5, 2, 1, 'ba69f5ee253e9823df6d8cd4a2992c42', '71dfd05d430324c69eaabb26e8faa781'),
+(6, 'TÃªnis Olympikus', 'Preto', '43', 159.99, 3, 1, 2, '49ace1e35f660b4f9dcc9513984fe484', '600edbb921334f2916974f266e8b286f'),
+(7, 'Camisa bonita nuuuu', 'Azul', 'G', 179.99, 4, 3, 1, '8fa8dbd62d6892dfaf06f8fd023bf89b', '52cc3e338a7237cdd60dfc65cebc3546');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipo`
+--
+
+INSERT INTO `tipo` (`codigo`, `nome`) VALUES
+(1, 'Roupa'),
+(2, 'CalÃ§ado'),
+(3, 'AcessÃ³rio');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `codigo` int(5) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`codigo`, `login`, `senha`) VALUES
+(1, 'admin', '123');
+
+--
+-- Restrições para as tabelas dumpadas
+--
+
+--
+-- Restrições para a tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`codmarca`) REFERENCES `marca` (`codigo`),
+  ADD CONSTRAINT `produto_ibfk_2` FOREIGN KEY (`codcategoria`) REFERENCES `categoria` (`codigo`),
+  ADD CONSTRAINT `produto_ibfk_3` FOREIGN KEY (`codtipo`) REFERENCES `tipo` (`codigo`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
